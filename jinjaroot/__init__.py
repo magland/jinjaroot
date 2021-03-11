4ffd792881fd4b57370e231c9fe8cb6e72b61a03
Jeremy Magland <jeremy.magland@gmail.com>
2021-03-11 15:04:22 -0500

    update cli

--- jinjaroot/__init__.py
+++ jinjaroot/__init__.py
@@ -1,1 +1,1 @@
-from .cli import cli
+from .generate import generate, verify
--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     packages=find_packages(),
     scripts=['bin/jinjaroot'],
-    install_requires=['pyyaml', 'jinja2']
+    install_requires=['pyyaml', 'jinja2', 'click']
 )
